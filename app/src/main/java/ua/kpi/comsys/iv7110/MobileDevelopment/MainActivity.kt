5def45b3e92afbf9a5057584091e6883625233b4
Kapibarabanka <yavarvara33@gmail.com>
2020-10-10 10:50:47 +0300

    Added lab1

---
+++ app/src/main/java/ua/kpi/comsys/iv7110/MobileDevelopment/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.kpi.comsys.iv7110.MobileDevelopment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
